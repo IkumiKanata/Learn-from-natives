b89c9852ad03322aa01f2a41d4ecaaad4755cd22
IkumiKanata <i.kanata0531@gmail.com>
2021-01-19 14:41:56 +0900

    email changes

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <h1>First Post!</h1>
         <Link href="/">
           <button>Back to home</button>
         </Link>
